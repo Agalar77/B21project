13027a35e416bda2f0f38554757f1e2aff0ac7df
Agalar77 <agalaraliyev7@gmail.com>
2020-11-10 12:41:36 -0500

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        // B21 class
+        System.out.println("Commit");
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello");
+        // B21 is good at IntelliJ
+
+
+
+
+
+    }
+}
